96c894394fc1b81b0db239958804616b927daab7
Shivanshu999 <shivanshuawasthi4@gmail.com>
2026-09-03 18:58:35 +0530

    6th_day

--- 24_file_handling/index.ts
+++ 24_file_handling/index.ts
@@ -1,1 +1,1 @@
-console.log('hello broos')
+//hello golang
--- 24_file_handling/main.go
+++ 24_file_handling/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"os"
 )
 
@@ -67,14 +69,55 @@ func main() {
 
 	//create file
 
-	f, err := os.Create("index.ts")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
+	// f, err := os.Create("index.ts")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer f.Close()
 	// f.WriteString("console.log('hello world')")
 
 	// bytes := []byte("console.log('hello broos')")
 	// f.Write(bytes)
 
+	//read and wite to another file (streaming fashion)
+
+	sourceFile, err := os.Open("example.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer sourceFile.Close()
+
+	destFile, err := os.Create("index.ts")
+	if err != nil {
+		panic(err)
+	}
+	defer destFile.Close()
+
+	reader := bufio.NewReader(sourceFile)
+	writer := bufio.NewWriter(destFile)
+
+	for {
+		b, err := reader.ReadByte()
+		if err != nil {
+			if err.Error() != "EOF" {
+				panic(err)
+			}
+			break
+		}
+		e := writer.WriteByte(b)
+		if e != nil {
+			panic(e)
+		}
+	}
+	writer.Flush()
+	fmt.Println("written to new file successfully")
+
+	//delete file
+
+	// err := os.Remove("index.ts")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println("file deleted successfully")
 }
